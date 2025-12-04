c0dfe92271018c4e9c0c8aa12c08a48457c04f0c
hssindustri <hsskemenperin@gmail.com>
2025-12-04 13:49:17 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT.appspot.com",
-    messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID"
-};
-const app = firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
-const db = firebase.firestore();
-// Init users dari localStorage
-let users = JSON.parse(localStorage.getItem('matcenUsers')) || [];
 let currentUser = null;
 
 // Modal functions
@@ -188,6 +175,7 @@ auth.onAuthStateChanged((user) => {
     }
 });
 </script>
+
 
 
 
